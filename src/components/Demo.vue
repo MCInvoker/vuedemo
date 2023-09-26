2ad1456c3c69ebaf10acd97e16334f51170b0f4e
chenshouchao <shouchao.chen@bkunyun.com>
2023-09-26 18:05:02 +0800

    vuedemo

---
+++ src/components/Demo.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <!-- 渲染 Button 类型的组件 -->
+        <element-comp type="button" @click="handleButtonClick">Click Me</element-comp>
+
+        <!-- 渲染 Input 类型的组件 -->
+        <element-comp type="input" v-model="inputValue"></element-comp>
+
+        <!-- 渲染 Tag 类型的组件 -->
+        <element-comp type="tag">Tag content</element-comp>
+    </div>
+</template>
+  
+<script>
+import ElementComp from './ElementComp.vue';
+
+export default {
+    components: {
+        ElementComp,
+    },
+    data() {
+        return {
+        inputValue: '',
+        };
+    },
+    methods: {
+        handleButtonClick(e) {
+            console.log(e)
+        // 处理按钮点击事件的逻辑
+        },
+    },
+};
+</script>
+  
---
+++ src/components/ElementComp.vue
@@ -0,0 +1,33 @@
+<template>
+  <component :is="componentType" v-bind="$attrs" v-on="$listeners">
+    <slot></slot>
+  </component>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'ElementComp',
+  props: {
+    type: {
+      type: String,
+      required: true,
+    },
+  },
+  computed: {
+    componentType() {
+      switch (this.type) {
+        case 'button':
+          return 'el-button';
+        case 'input':
+          return 'el-input';
+        case 'tag':
+          return 'el-tag';
+        default:
+          return null;
+      }
+    },
+  },
+});
+</script>
